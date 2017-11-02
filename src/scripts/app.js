9d0f6dd63c330dc0ecc7dd23689f362799baeab0
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2017-11-02 18:29:43 -0200

    Protótipo pronto

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,6 +2,40 @@ $(document).ready(function() {
 
 	'use strict';
 
+	var restaurants = [
+		'Benedito <span>(Tente de novo)</span>',
+		'Madero <span>(2x / mês)</span>',
+		'SmartAurant',
+		'Mulher que Dança',
+		'Karina\'s',
+		'Cantinho <br /> da Gula',
+		'Mineiro',
+		'Jasmin',
+		'Tio da Bocha',
+		'Vilinha',
+		'Espetinho',
+		'Atravessar a rua',
+		'Hoje não quero ajudar!',
+		'Purê <span(se tiver em 4 pessoas)</span>',
+		'Viado Paulista',
+		'Piscina',
+		'3B',
+		'Guri',
+		'Si Señor <span>(VR cheio)</span>',
+		'Brau',
+		'Kilinho',
+		'PF'
+	];
 
+	function randomRestaurant() {
+		var i = Math.floor( Math.random() * restaurants.length );
+		console.log(i);
+
+		var h1 = document.querySelector('h1');
+
+		h1.innerHTML = restaurants[i];
+	}
+
+	randomRestaurant();
 
 });
