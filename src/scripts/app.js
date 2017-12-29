0dd4940889212e88141ab0b240a14c607933675e
DouglasAmarelo <d.lopes.nascimento@gmail.com>
2017-12-29 10:30:23 -0200

    CTA para sorteio

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,120 +1,131 @@
-// $(document).ready(function() {
+'use strict';
 
-	'use strict';
+// Lista de background images
+var images = [
+	'background-1.jpg',
+	'background-2.jpg',
+	'background-3.jpg',
+	'background-4.jpg',
+	'background-5.jpg',
+	'background-6.jpg',
+	'background-7.jpg'
+];
 
-(function() {
-	// Lista de background images
-	var images = [
-		'background-1.jpg',
-		'background-2.jpg',
-		'background-3.jpg',
-		'background-4.jpg',
-		'background-5.jpg',
-		'background-6.jpg',
-		'background-7.jpg'
-	];
+// Lista de restaurantes
+var restaurants = [
+	'Benedito <span>(Tente de novo)</span>',
+	'Madero <span>(máximo 2x / mês)</span>',
+	'SmartAurant',
+	'Mulher que Dança',
+	'Karina\'s',
+	'Cantinho <br /> da Gula',
+	'Mineiro',
+	'Jasmim',
+	'Tio da Bocha',
+	'Vilinha',
+	'Espetinho',
+	'Atravessar a rua',
+	'Purê <span(se tiver em 4 pessoas)</span>',
+	'ViRado Paulista',
+	'Piscininha',
+	'3B',
+	'Guri',
+	'Si Señor <span>(só com o VR cheio)</span>',
+	'Brau',
+	'Algum Kilinho',
+	'Algum PF',
+	'Vapiano',
+	'PCC',
+	'Pastel da Maria',
+	'Prime Burger',
+	'Bolados',
+	'Cheese Dog',
+	'New Dog',
+	'Espeto Paulista',
+	'Joakins',
+	'Buger Lab',
+	'Padaria Milionária',
+	'The Fifties',
+	'Dog Milionário',
+	'SubWay',
+	'La Pergoletta',
+	'Butcher\'s'
+];
 
-	// Lista de restaurantes
-	var restaurants = [
-		'Benedito <span>(Tente de novo)</span>',
-		'Madero <span>(máximo 2x / mês)</span>',
-		'SmartAurant',
-		'Mulher que Dança',
-		'Karina\'s',
-		'Cantinho <br /> da Gula',
-		'Mineiro',
-		'Jasmim',
-		'Tio da Bocha',
-		'Vilinha',
-		'Espetinho',
-		'Atravessar a rua',
-		'Purê <span(se tiver em 4 pessoas)</span>',
-		'ViRado Paulista',
-		'Piscininha',
-		'3B',
-		'Guri',
-		'Si Señor <span>(só com o VR cheio)</span>',
-		'Brau',
-		'Algum Kilinho',
-		'Algum PF',
-		'Vapiano',
-		'PCC',
-		'Pastel da Maria',
-		'Prime Burger',
-		'Bolados',
-		'Cheese Dog',
-		'New Dog',
-		'Espeto Paulista',
-		'Joakins',
-		'Buger Lab',
-		'Padaria Milionária',
-		'The Fifties',
-		'Dog Milionário',
-		'SubWay',
-		'La Pergoletta',
-		'Butcher\'s'
-	];
+// Variáveis
+var restaurante       = document.querySelector('.restaurante');
+var opcoes            = document.querySelector('.opcoes');
+var numTentativas     = document.querySelector('.tentativas');
+var docBody           = document.querySelector('body');
+var numTentativasText = parseInt(numTentativas.textContent);
+var clickCounter      = 0;
+var ctaSortear        = docBody.querySelector('.sortear');
+
+// Função genérica de sorteio
+function randomAll( itemsList ) {
+	var i = Math.floor( Math.random() * itemsList.length );
+
+	return itemsList[i];
+}
+
+// Exibe a quantidade de restaurantes da lista
+opcoes.innerText = restaurants.length;
+
+// Sorteia as imagens de fundo
+function randomBackground(){ return randomAll( images ); }
 
 
-	// Variáveis
-	var restaurante       = document.querySelector('.restaurante');
-	var opcoes            = document.querySelector('.opcoes');
-	var numTentativas     = document.querySelector('.tentativas');
-	var docBody           = document.querySelector('body');
-	var numTentativasText = parseInt(numTentativas.textContent);
-	var clickCounter      = 0;
+// Sorteia o restaurante
+function randomRestaurant() { return randomAll( restaurants ); }
 
-	// Função genérica de sorteio
-	function randomAll( itemsList ) {
-		var i = Math.floor( Math.random() * itemsList.length );
 
-		return itemsList[i];
+// Sorteia e atualiza conteudo da página
+function updateContent() {
+
+	// Altera a imagem de fundo
+	docBody.style.backgroundImage = 'url("images/' + randomBackground() + '")';
+
+	if ( clickCounter <= 2 ) {
+
+		// Altera nome do restaurante
+		restaurante.innerHTML = randomRestaurant();
+
+		clickCounter +=1;
+		numTentativasText -= 1;
+		numTentativas.textContent = numTentativasText;
 	}
-
-	// Exibe a quantidade de restaurantes da lista
-	opcoes.innerText = restaurants.length;
-
-	// Sorteia as imagens de fundo
-	function randomBackground(){ return randomAll( images ); }
-
-
-	// Sorteia o restaurante
-	function randomRestaurant() { return randomAll( restaurants ); }
-
-
-	// Sorteia e atualiza conteudo da página
-	function updateContent() {
-
-		// Altera a imagem de fundo
-		docBody.style.backgroundImage = 'url("images/' + randomBackground() + '")';
-
-		if ( clickCounter <= 2 ) {
-
-			// Altera nome do restaurante
-			restaurante.innerHTML = randomRestaurant();
-
-			clickCounter +=1;
-			numTentativasText -= 1;
-			numTentativas.textContent = numTentativasText;
-		}
-		else {
-			restaurante.innerHTML = '<span class="error">Suas tentativas terminaram.</span>';
-		}
+	else {
+		restaurante.innerHTML = '<span class="error">Suas tentativas terminaram.</span>';
 	}
+}
 
+// Sorteia ao clicar na página ou ao pressionar a 'barra de espaço'
+// document.addEventListener('click', updateContent);
+// document.addEventListener('keyup', function(e){
+// 	e.preventDefault();
 
-	// Sorteia ao clicar na página ou ao pressionar a 'barra de espaço'
-	document.addEventListener('click', updateContent);
-	document.addEventListener('keyup', function(e){
-		e.preventDefault();
-
-		if( e.keyCode === 32 ) { updateContent(); }
-	});
-
-
-	// Quando a página inicia
-	restaurante.innerHTML = randomRestaurant();
-
-})();
-
+// 	if( e.keyCode === 32 ) { updateContent(); }
 // });
+
+// Sorteia ao clicar no botão
+ctaSortear.addEventListener('click', function(event) {
+	event.preventDefault();
+
+	this.classList.add('is--active');
+
+	if ( clickCounter > 2 ) {
+
+		this.classList.add('restart');
+
+		clickCounter++;
+	}
+
+	if ( clickCounter === 5 && this.classList.contains('restart') ) {
+		window.location.href = '/';
+	}
+
+	updateContent();
+});
+
+// Quando a página inicia
+// restaurante.innerHTML = randomRestaurant();
--- src/scripts/app_v2.js
+++ src/scripts/app_v2.js
@@ -1,56 +1,68 @@
-// 
+//
 
 var $ = require("../../node_modules/jquery");
 
 (function () {
-    var self = this;
+	var self = this;
 
-    self.init = function () {
-        self.generateBgMap();
-        self.setSearchBox();
-    };
+	self.init = function () {
+		self.generateBgMap();
+		self.setSearchBox();
+	};
 
-    self.setSearchBox = function () {
-        var input = document.getElementById('your_address__input');
-        var searchBox = new google.maps.places.SearchBox(input);
+	self.setSearchBox = function () {
+		var input = document.getElementById('your_address__input');
+		var searchBox = new google.maps.places.SearchBox(input);
 
-        searchBox.addListener('places_changed', function () {
-            var places = searchBox.getPlaces();
+		searchBox.addListener('places_changed', function () {
+			var places = searchBox.getPlaces();
 
-            if (places.length == 0) {
-                return;
-            }
+			if (places.length == 0) {
+				return;
+			}
 
-            var coordsLat = places[0].geometry.location.lat(),
-                coordsLng = places[0].geometry.location.lng(),
-                coords = {lat: coordsLat, lng: coordsLng};
+			var coordsLat = places[0].geometry.location.lat(),
+				coordsLng = places[0].geometry.location.lng(),
+				coords = {lat: coordsLat, lng: coordsLng};
 
-            // console.log(coords);
+			// console.log(coords);
 
-            self.generateBgMap(coords);
-        });
-    };
+			self.generateBgMap(coords);
+		});
+	};
 
-    self.generateBgMap = function (coordinates) {
-        var map,
-            coord = coordinates || {lat: -23.584293, lng: -46.676080};
+	self.generateBgMap = function (coordinates) {
+		var map,
+			coord = coordinates || {lat: -23.584293, lng: -46.676080};
 
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: coord,
-                zoom: 18
-            });
+			map = new google.maps.Map(document.getElementById('map'), {
+				center: coord,
+				zoom: 18
+			});
 
-            if (coordinates) {
-                var service = new google.maps.places.PlacesService(map);
-                service.radarSearch({
-                    location: coord,
-                    radius: 1000,
-                    types: ['restaurant']
-                }, function(a, b) {
-                    console.log(a, b);
-                });
-            }
-    }
+			if (coordinates) {
+				var service = new google.maps.places.PlacesService(map);
+				service.radarSearch({
+					location: coord,
+					radius: 1000,
+					types: ['restaurant']
+				}, function(a, b) {
+					console.log(a, b);
+				});
+			}
+	}
 
-    self.init();
+	self.addMarker = function(place) {
+		var marker = new google.maps.Marker({
+		  map: map,
+		  position: place.geometry.location,
+		  icon: {
+			url: 'https://developers.google.com/maps/documentation/javascript/images/circle.png',
+			anchor: new google.maps.Point(10, 10),
+			scaledSize: new google.maps.Size(10, 17)
+		  }
+		});
+	}
+
+	self.init();
 })($);
